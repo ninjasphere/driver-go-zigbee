7fc73547e2df8302ef77a45828a38637b09694fe
Jon Seymour <jon.seymour@gmail.com>
2014-10-07 20:45:25 +1100

    Generalise test script.

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,4 +1,38 @@
 #!/usr/bin/env bash
-name=$1
 
-tail +2 testdata/${name}.json | mosquitto_pub -t "$(head -1 testdata/${name}.json)" -s
+channel() {
+    local device=$1
+    local channel=$2
+    echo "\$device/${device}/channel/${channel}"
+}
+
+payload() {
+    method=$1
+
+cat <<EOF
+{"params":[],"method":"$method","jsonrpc":"2.0","time":1411702470066,"id":6132576}
+EOF
+
+}
+
+run-test() {
+    local channel=$1
+    local method=$2
+    local device=${3:-${TEST_DEVICE}}
+
+    if test -n "$channel" 
+    then
+	if test -z "$device"
+	then
+	    echo "export TEST_DEVICE={ninja-device-id}" 1>&2
+	    exit 1
+	fi
+
+	payload $method | mosquitto_pub -t "$(channel $device $channel)" -s
+    else
+	echo "usage: run-test.sh channel method [device]" 1>&2
+	exit 1
+    fi
+}
+
+run-test "$@"
