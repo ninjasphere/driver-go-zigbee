548c3a7f2333f67c3d778a50eef052c7afc2bbff
Jon Seymour <jon.seymour@gmail.com>
2014-10-09 00:05:22 +1100

    Generalise test script support.
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-channel() {
-    local device=$1
-    local channel=$2
-    echo "\$device/${device}/channel/${channel}"
+topic() {
+    local dtype=$1
+    local device=$2
+    local ctype=$3
+    local channel=$4
+    echo "\$${dtype}/${device}/${ctype}/${channel}"
 }
 
 payload() {
@@ -15,24 +17,57 @@ EOF
 
 }
 
-run-test() {
-    local channel=$1
-    local method=$2
-    local device=${3:-${TEST_DEVICE}}
+main()
 
-    if test -n "$channel" 
-    then
-	if test -z "$device"
-	then
-	    echo "export TEST_DEVICE={ninja-device-id}" 1>&2
-	    exit 1
-	fi
+{
+       _channel() {
+	   local channel=$1
+	   local device=${2:-${TEST_DEVICE}}
 
-	payload $method | mosquitto_pub -h ${TEST_HOST:-localhost} -t "$(channel $device $channel)" -s
-    else
-	echo "usage: run-test.sh channel method [device]" 1>&2
-	exit 1
-    fi
+	   if test -n "$channel" 
+	   then
+	       if test -z "$device"
+	       then
+		   echo "export TEST_DEVICE={ninja-device-id}" 1>&2
+		   exit 1
+	       fi
+
+	       mosquitto_pub -h ${TEST_HOST:-localhost} -t "$(topic device $device channel $channel)" -s
+	   else
+	       echo "usage: run-test.sh channel {channel} [{device}]" 1>&2
+	       exit 1
+	   fi
+       }
+
+       _driver() {
+	   local node=$1
+	   local driver=$2
+
+	   mosquitto_pub -h ${TEST_HOST:-localhost} -t "$(topic node $node driver $driver)" -s
+       }
+
+       _payload() {
+	   simple() {
+	       local method=$1
+
+cat <<EOF
+{"params":[],"method":"$method","jsonrpc":"2.0","time":1411702470066,"id":6132576}
+EOF
+           }
+	   
+	   startPairing() {
+	       local timeout=$1
+
+cat <<EOF
+{"params":[$timeout],"method":"startPairing","jsonrpc":"2.0","time":1411702470066,"id":6132576}
+EOF
+	   }
+	   "$@"
+       }
+
+       cmd=$1
+       shift 1
+       _$cmd "$@"
 }
 
-run-test "$@"
+main "$@"
