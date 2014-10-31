5c702f1496f0eedf17b56abc812aaa6f7949febd
Jon Seymour <jon@ninjablocks.com>
2014-10-31 13:30:53 +1100

    New logger initialization pattern.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- debug.go
+++ debug.go
@@ -4,15 +4,9 @@ package main
 
 import (
 	"github.com/bugsnag/bugsnag-go"
-	"github.com/juju/loggo"
-	"github.com/ninjasphere/go-ninja/logger"
 )
 
-var debug = logger.GetLogger("").Warningf
-
 func init() {
-	logger.GetLogger("").SetLogLevel(loggo.DEBUG)
-
 	bugsnag.Configure(bugsnag.Configuration{
 		APIKey:       "6145ec8c75d22e6236758aca31247a63",
 		ReleaseStage: "development",
--- release.go
+++ release.go
@@ -4,13 +4,9 @@ package main
 
 import (
 	"github.com/bugsnag/bugsnag-go"
-	"github.com/juju/loggo"
-	"github.com/ninjasphere/go-ninja/logger"
 )
 
 func init() {
-	logger.GetLogger("").SetLogLevel(loggo.INFO)
-
 	bugsnag.Configure(bugsnag.Configuration{
 		APIKey:       "6145ec8c75d22e6236758aca31247a63",
 		ReleaseStage: "production",
