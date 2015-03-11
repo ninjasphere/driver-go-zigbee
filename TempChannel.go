a2a4d30e9e1edd8a6a4f9942645a4e3fc4aa46b9
☃ Elliot Shepherd <elliot@ninjablocks.com>
2015-03-11 16:25:01 +1100

    Only poll temperature once a minute (as they are often battery devices)

--- TempChannel.go
+++ TempChannel.go
@@ -58,7 +58,7 @@ func (c *TempChannel) init() error {
 			if err != nil {
 				log.Errorf("Failed to poll for Temperature %s", err)
 			}
-			time.Sleep(10 * time.Second)
+			time.Sleep(1 * time.Minute)
 		}
 	}()
 
