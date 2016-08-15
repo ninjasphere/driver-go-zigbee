014a377200c9e977dd1d6baf12781e61a179a304
Jon Seymour <jon@wildducktheories.com>
2016-08-15 13:01:41 +1000

    Fix dependency on juju/loggo
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -26,7 +26,12 @@ export GOPATH="$(pwd)/.gopath"
 
 # move the working path and build
 cd .gopath/src/github.com/${OWNER}/${PROJECT_NAME} &&
-go get -d -v ./... &&
+go get -d -v ./...
+
+# fixup go dependencies
+GOOS= GOARCH= go get github.com/tools/godep
+PATH=${GOPATH}/bin:${PATH} godep restore 
+
 go build -ldflags "-X main.GitCommit ${GIT_COMMIT}${GIT_DIRTY}" -o ${BIN_NAME} && (
 # building the master branch on ci
 if [ "$BUILDBOX_BRANCH" = "master" ]; then
